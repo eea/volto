b1c73c4d2ebdb03e3287aee83b5476a1d1f1327c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-06-22 22:07:28 +0300

    Tweak Form.jsx to use settings.defaultBlockType instead of 'text' for default block type

--- src/components/manage/Form/Form.jsx
+++ src/components/manage/Form/Form.jsx
@@ -31,6 +31,7 @@ import { v4 as uuid } from 'uuid';
 import { Portal } from 'react-portal';
 
 import { EditBlock, Icon, Field } from '@plone/volto/components';
+import { settings } from '~/config';
 import dragSVG from '@plone/volto/icons/drag.svg';
 
 import {
@@ -188,7 +189,7 @@ class Form extends Component {
             '@type': 'title',
           },
           [ids.text]: {
-            '@type': 'text',
+            '@type': settings.defaultBlockType,
           },
         };
       }
@@ -283,7 +284,7 @@ class Form extends Component {
           ...this.state.formData[blocksFieldname],
           [id]: value || null,
           [idTrailingBlock]: {
-            '@type': 'text',
+            '@type': settings.defaultBlockType,
           },
         },
         [blocksLayoutFieldname]: {
@@ -361,7 +362,7 @@ class Form extends Component {
               insert,
             ),
             id,
-            ...(type !== 'text' ? [idTrailingBlock] : []),
+            ...(type !== settings.defaultBlockType ? [idTrailingBlock] : []),
             ...this.state.formData[blocksLayoutFieldname].items.slice(insert),
           ],
         },
@@ -370,9 +371,9 @@ class Form extends Component {
           [id]: {
             '@type': type,
           },
-          ...(type !== 'text' && {
+          ...(type !== settings.defaultBlockType && {
             [idTrailingBlock]: {
-              '@type': 'text',
+              '@type': settings.defaultBlockType,
             },
           }),
         },
@@ -571,7 +572,7 @@ class Form extends Component {
       e.preventDefault();
     }
     if (e.key === 'Enter' && !disableEnter) {
-      this.onAddBlock('text', index + 1);
+      this.onAddBlock(settings.defaultBlockType, index + 1);
       e.preventDefault();
     }
   }

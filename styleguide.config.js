92f9c52ee2aab0f38a2590195f43abbbfafc667a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-06-30 22:02:57 +0300

    WIP on giving styleguide a go

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,27 +1,46 @@
+const path = require('path');
+const utils = require('./razzle-config-utils.js');
+const projectRootPath = path.resolve('.');
+const registry = new utils.AddonConfigurationRegistry(projectRootPath);
+
 module.exports = {
   components: 'src/components/**/*.jsx',
-  ignore: ['**/*.test.jsx', '**/Contents/Contents*jsx', '**/Tile/Tile.jsx'],
+  ignore: [
+    '**/*.test.jsx',
+    '**/Contents/Contents*jsx',
+    '**/Tile/Tile.jsx',
+    '**/AnchorPlugin/index.jsx',
+    '**/theme/AppExtras/AppExtras.jsx',
+    '**/Widgets/SchemaWidgetFieldset.jsx',
+    '**/ObjectBrowser.jsx',
+  ],
   verbose: true,
   title: 'Volto Style Guide',
   showCode: true,
   showUsage: true,
   sections: [
-    {
-      name: 'Introduction',
-      content: 'docs/introduction.md',
-    },
+    //   // {
+    //   //   name: 'Introduction',
+    //   //   content: 'docs/source/index.md',
+    //   // },
     {
       name: 'Theme',
       components: 'src/components/theme/**/*.jsx',
-      content: 'docs/theme.md',
+      content: 'styleguide/theme.md',
     },
-    {
-      name: 'Manage',
-      components: 'src/components/manage/**/*.jsx',
-      content: 'docs/manage.md',
-    },
+    //   // {
+    //   //   name: 'Manage',
+    //   //   components: 'src/components/manage/**/*.jsx',
+    //   //   content: 'docs/manage.md',
+    //   // },
   ],
   webpackConfig: {
+    resolve: {
+      alias: {
+        '@plone/volto': `${registry.voltoPath}/src`,
+        'load-volto-addons': `${projectRootPath}/dummy-addons-loader.js`,
+      },
+    },
     module: {
       rules: [
         {
@@ -30,7 +49,7 @@ module.exports = {
           loader: 'babel-loader',
         },
         {
-          test: /\.(woff|woff2|ttf|eot|svg|png|gif|jpg)(\?v=\d+\.\d+\.\d+)?$/,
+          test: /\.(css|woff|woff2|ttf|eot|svg|png|gif|jpg)(\?v=\d+\.\d+\.\d+)?$/,
           loader: 'url-loader',
           options: {
             limit: 10000,

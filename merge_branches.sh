6f50f6379d11e6fe9782ce700157ba6cfe941f56
Alin Voinea <contact@avoinea.com>
2020-08-27 12:20:58 +0300

    Cleanup merge script

--- merge_branches.sh
+++ merge_branches.sh
@@ -10,7 +10,6 @@ git merge volto/master
 git merge volto/export_components
 git merge volto/eea-dx-fields-validation
 git merge volto/eea-dx-cpanel-layout
-git merge volto/eea-fix-custom-action-headers
 git merge volto/form_context_clean_breaking_with_hoc
 git merge volto/addons_customize
 git merge volto/safe_apply_config
@@ -18,8 +17,8 @@ git merge volto/object-widget-final
 git merge volto/upload_content
 
 # Already merged to Volto master
-# git merge --no-ff volto/default-block-rebased
-# git merge --no-ff volto/fix_toolbar_ssr
+# git merge volto/default-block-rebased
+# git merge volto/fix_toolbar_ssr
 # git merge volto/wysiwyg_desc_styling
 # git merge volto/disable_submit
 # git merge volto/fix_querystring_ssr_edit
@@ -31,3 +30,4 @@ git merge volto/upload_content
 # git merge volto/separate_less_loader
 # git merge volto/eea-fix-get-blocks
 # git merge volto/internalApiPath_in_url_helpers
+# git merge volto/eea-fix-custom-action-headers

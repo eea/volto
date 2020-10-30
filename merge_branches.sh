6c0f3e96a93691828250d1c5ba4e00c9730cc3aa
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-30 16:27:30 +0200

    Add merge script

---
+++ merge_branches.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -x
+
+git remote add volto https://github.com/plone/volto.git
+git fetch --all
+git pull
+
+# git merge volto/master
+git merge volto/multi_block_copypaste
+git merge volto/ssr_errors
+git merge volto/is_backend_resource
